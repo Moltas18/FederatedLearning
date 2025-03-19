Fixed dataparsing and seed. Still there is an issue with the data normalization which needs to be adressed.

Data/data.py
@@ -1,39 +1,95 @@
-from torch.utils.data import DataLoader
-from flwr_datasets import FederatedDataset
-import torchvision.transforms as transforms
-from typing import Union
-from flwr_datasets.partitioner import Partitioner
+# Torch imports
 import torch
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
 
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-torch.cuda.manual_seed_all(42)  # If using multi-GPU
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+# Flower imports
+from flwr_datasets import FederatedDataset
+from flwr_datasets.partitioner import Partitioner
+
+# Other imports
+import numpy as np
+from typing import Union, Sequence
+from datasets import DatasetDict
 
 class Data:
+
     def __init__(self,
-                 batch_size: int,
+                 batch_size: Union[str, int, np.integer, torch.Tensor],
                  partitioner: Union[int, Partitioner],
+                 partition_size: Union[int, np.integer, torch.Tensor]=None,
                  dataset: str = "cifar10",
-                 seed: int = 42,
-                 val_size: float = 0.2,
-                 val_test_batch_size: int=64) -> None:
+                 seed: Union[int, np.integer] = 42,
+                 include_test_set = False,
+                 val_size: float = 0.5,
+                 val_test_batch_size: Union[int, np.integer, torch.Tensor]=64,
+                 normalization_means: Sequence[float] = (0.5, 0.5, 0.5),
+                 normalization_stds: Sequence[float] = (0.5, 0.5, 0.5)
+                 ) -> None:
         
         self.dataset = dataset
         self._batch_size = batch_size  # The batch size for training
-        self._val_test_batch_size = val_test_batch_size  # The batch size for validation and testing
-        self._partitioner = partitioner
-        self._seed = seed
+        self._val_test_batch_size = int(val_test_batch_size)  # The batch size for validation and testing
+        self._partitioner = partitioner 
+        self._partition_size = partition_size
+        self._seed = int(seed)
         self._val_size = val_size
-        self.fds = FederatedDataset(dataset=self.dataset,
-                                    partitioners={"train": self._partitioner},
-                                    seed=self._seed)
-        
-        
-        # Create test loader before partitioning the data, so that the test data is the same for all partitions
-        self.create_test_loader()
+        self._include_test_set = include_test_set
 
+        assert len(normalization_means) == 3 and len(normalization_stds) == 3, \
+            "Normalization parameters must have exactly 3 values (for RGB channels)."
+        
+        self.normalization_means = normalization_means
+        self.normalization_stds = normalization_stds
+
+        
+
+        # Validate partitioner type
+        if not isinstance(self._partitioner, (int, np.integer, torch.Tensor, Partitioner)):
+            print(type(self._partitioner))
+            raise ValueError("partitioner must be either an int, a numpy integer, a torch tensor, or a Partitioner instance")
+        
+        # Create the apply_transformation function
+        # self.apply_transforms = self._create_apply_transforms()
+        
+        # Create the fds 
+        self._create_fds()
+
+        # Create test loader before partitioning the data, so that the test data is the same for all partitions
+        self._create_test_loader()
+
+    def _create_fds(self):
+        
+        # If the partitioner is an int, the dataset will be scaled down to only include many enough images.
+        if isinstance(self._partitioner, (int, np.integer, torch.Tensor)):
+        
+            if not isinstance(self._partition_size, (int, np.integer, torch.Tensor)):
+                raise ValueError("partition_size must be an int if partitioner is an int")
+            
+            else:
+                self._partition_size = int(self._partition_size)
+                self._partitioner = int(self._partitioner)
+                total_data_size = self._partition_size * self._partitioner
+
+
+                self.fds = FederatedDataset(dataset=self.dataset,
+                                       partitioners={"train": self._partitioner},
+                                       seed=self._seed,
+                                       preprocessor=lambda d: self.trim_dataset(d, total_data_size),
+                                       shuffle=False)
+
+        # If partitioner is a Partitioner, this will be passed forward to the FederatedDataset   
+        else:
+            self.fds = FederatedDataset(dataset=self.dataset,
+                                        partitioners={"train": self._partitioner},
+                                        seed=self._seed,
+                                        shuffle=False)
+
+    @staticmethod
+    def trim_dataset(dataset_dict: DatasetDict, n: int) -> DatasetDict:
+        """Trims each dataset in DatasetDict to the first N samples."""
+        return DatasetDict({k: v.select(range(min(n, len(v)))) for k, v in dataset_dict.items()})
+    
     @staticmethod
     def apply_transforms(batch):
         """Apply PyTorch transforms to the dataset."""
@@ -45,17 +101,21 @@ class Data:
         batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
         return batch
 
-    def get_batch_size(self, partition_train_test):
+    def _get_batch_size(self, partition_train_test):
         """Determine the batch size for training, validation, and testing."""
         trainset_size = len(partition_train_test["train"])
 
         if self._batch_size == "full" or self._batch_size > trainset_size:
             return trainset_size, self._val_test_batch_size  # Full train batch, fixed val/test batch
         else:
-            return self._batch_size, self._val_test_batch_size  # Custom train batch, fixed val/test batch
+            return int(self._batch_size), self._val_test_batch_size  # Custom train batch, fixed val/test batch
 
-    def create_test_loader(self) -> None:
+    def _create_test_loader(self) -> None:
         """Create test data loader before partitioning."""
+        if not self._include_test_set:
+            self.testloader = DataLoader(EmptyDataset(), batch_size=self._val_test_batch_size)
+            return
+
         testset = self.fds.load_split("test").with_transform(self.apply_transforms)
         self.testloader = DataLoader(testset, batch_size=self._val_test_batch_size)
 
@@ -70,10 +130,54 @@ class Data:
         partition_train_test = partition_train_test.with_transform(self.apply_transforms)
         
         # Get batch sizes
-        batch_size_train, batch_size_val = self.get_batch_size(partition_train_test)
+        batch_size_train, batch_size_val = self._get_batch_size(partition_train_test)
         
         # Create DataLoaders
-        trainloader = DataLoader(partition_train_test["train"], batch_size=batch_size_train, shuffle=True)
-        valloader = DataLoader(partition_train_test["test"], batch_size=batch_size_val)
+        trainloader = DataLoader(partition_train_test["train"], batch_size=batch_size_train, shuffle=False)
+        valloader = DataLoader(partition_train_test["test"], batch_size=batch_size_val, shuffle=False)
 
         return trainloader, valloader, self.testloader
+
+    def get_config_dict(self):
+        """Return a dictionary with all parameters needed to recreate the dataset."""
+        config_dict = {
+            "dataset": self.dataset,
+            "batch_size": self._batch_size,
+            "val_test_batch_size": self._val_test_batch_size,
+            "partitioner": self._partitioner if isinstance(self._partitioner, int) else self._partitioner.__class__.__name__,
+            "partition_size": self._partition_size,
+            "seed": self._seed,
+            "val_size": self._val_size,
+            "include_test_set": self._include_test_set,
+            "normalization_means": list(self.normalization_means),
+            "normalization_stds": list(self.normalization_stds)
+        }
+        return config_dict
+    
+class EmptyDataset(Dataset):
+    """An empty dataset returning no samples."""
+    def __len__(self):
+        return 0
+
+    def __getitem__(self, idx):
+        raise IndexError("This dataset is empty.")
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    # Add the root directory to the sys.path
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+    d = Data(
+        batch_size=1,
+        partitioner= 10,
+        partition_size = 2,
+        val_size=0.5
+    )
+
+    trainloader, valloader, testloader = d.load_datasets(partition_id=0)
+
+    print(f"Number of samples in the training dataset: {len(trainloader.dataset)}")
+    print(f"Number of samples in the validation dataset: {len(valloader.dataset)}")
+

src/utils.py
@@ -10,13 +10,10 @@ from pathlib import Path
 from time import time 
 import json
 from tqdm import tqdm
+from typing import Union, List, Tuple, Sequence
+import random
 
 
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-torch.cuda.manual_seed_all(42)  # If using multi-GPU
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
 
 def timer(func):
     """
@@ -42,6 +39,16 @@ def timer(func):
         return result
 
     return wrap_func
+
+def set_global_seed(seed):
+    random.seed(seed)  # Python's random module
+    np.random.seed(seed)  # NumPy
+    torch.manual_seed(seed)  # PyTorch (CPU)
+    torch.cuda.manual_seed(seed)  # PyTorch (GPU)
+    torch.cuda.manual_seed_all(seed)  # PyTorch (all GPUs)
+    torch.backends.cudnn.deterministic = True  # Ensure deterministic behavior
+    torch.backends.cudnn.benchmark = False  # Disable auto-tuning
+    torch.use_deterministic_algorithms(True)
 
 def eval_weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     '''
@@ -132,9 +139,13 @@ def get_filenames(directory: Union[str, Path]) -> List[str]:
 
 def parse_run(run_path: Union[str, Path]) -> pd.DataFrame:
 
-        # Read config
-        config_path = run_path + 'run_config.jsonl'
-        config = read_from_file(config_path)[0]
+        # Read run_config
+        run_config_path = run_path + 'run_config.jsonl'
+        run_config = read_from_file(run_config_path)[0]
+        
+        # Read data config
+        data_config_path = run_path + 'data_config.jsonl'
+        data_config = read_from_file(data_config_path)[0]
 
         # Get the file names of the parameters saving (from each client)
         parameters_path = run_path + 'parameters/'
@@ -144,7 +155,7 @@ def parse_run(run_path: Union[str, Path]) -> pd.DataFrame:
         data = {
             'Server Round' : [],
             'Client ID': [],
-            'Batch Size': [],
+            'Actual Batch Size': [],
             'Num Batches': [],
             'Partition ID': [],
             'Initial Parameters': [],
@@ -157,7 +168,7 @@ def parse_run(run_path: Union[str, Path]) -> pd.DataFrame:
             for training in run_parameters:
                 data['Server Round'].append(training['run_info']['server_round'])
                 data['Client ID'].append(training['run_info']['client_id'])
-                data['Batch Size'].append(training['run_info']['batch_size'])
+                data['Actual Batch Size'].append(training['run_info']['batch_size']) # This is the actual batch size for this specific round!
                 data['Num Batches'].append(training['run_info']['num_batches'])
                 data['Partition ID'].append(training['run_info']['node_config']['partition-id'])
                 data['Initial Parameters'].append(training['parameters']['parameters_before_training'])
@@ -165,13 +176,26 @@ def parse_run(run_path: Union[str, Path]) -> pd.DataFrame:
 
         # Create dataframe
         df = pd.DataFrame(data)
-        df['Epochs'] = config['epochs']
-        df['Net'] = config['net']
-        df['Num Clients'] = config['num_clients']
-        df['Total Rounds'] = config['num_rounds']
-        df['Optimizer'] = config['optim_method']
-        df['Learning Rate'] = config['learning_rate']
-        
+
+        # Add info from the run_config
+        df['Epochs'] = run_config['epochs']
+        df['Net'] = run_config['net']
+        df['Num Clients'] = run_config['num_clients']
+        df['Total Rounds'] = run_config['num_rounds']
+        df['Optimizer'] = run_config['optim_method']
+        df['Learning Rate'] = run_config['learning_rate']
+
+        # Add info from the data_config. This info will be used to create a new instance of the Data class.
+        df['Dataset'] = data_config['dataset']
+        df['Data Batch Size'] = data_config['batch_size']
+        df['Val/Test Batch Size'] = data_config['val_test_batch_size']
+        df['Partitioner'] = data_config['partitioner']
+        df['Partition Size'] = data_config['partition_size']
+        df['Seed'] = data_config['seed']
+        df['Validation Size'] = data_config['val_size']
+        df['Include Test Set'] = data_config['include_test_set']
+        df['Normalization Means'] = [data_config['normalization_means']] * len(df)
+        df['Normalization Stds'] = [data_config['normalization_stds']] * len(df)  
         return df
 
 def plot_image_samples(images: torch.Tensor) -> None: 
@@ -288,9 +312,14 @@ def plot_run_results(metrics_path: str, config_path: str) -> None:
     plt.tight_layout()
     plt.show()
 
-def denormalize(img):
-        device = img.device
-        mean = torch.tensor([0.5, 0.5, 0.5], device=device).view(3, 1, 1)
-        std = torch.tensor([0.5, 0.5, 0.5], device=device).view(3, 1, 1)
-        return img * std + mean  # Reverse normalization
+def denormalize(img: torch.Tensor,
+                means: Sequence[float],
+                stds: Sequence[float]):
+    '''
+    Denormalizes the image (tensor) with respect to the means and stds.
+    '''
+    device = img.device
+    mean = torch.tensor(means, device=device).view(3, 1, 1)
+    std = torch.tensor(stds, device=device).view(3, 1, 1)
+    return img * std + mean  # Reverse normalization
     